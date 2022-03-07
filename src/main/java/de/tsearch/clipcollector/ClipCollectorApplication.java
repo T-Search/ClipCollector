d31392c54861607187eeba7d0e83b93b2f69e2e5
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-03-07 23:08:07 +0100

    Init spring project

---
+++ src/main/java/de/tsearch/clipcollector/ClipCollectorApplication.java
@@ -0,0 +1,13 @@
+package de.tsearch.clipcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClipCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/tsearch/clipcollector/ClipCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tsearch.clipcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
