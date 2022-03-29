76ad693af3f5bc312abafe9f2789b587c33605a7
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-03-30 01:20:08 +0200

    Replace batch task with schedule method

--- src/main/java/de/tsearch/clipcollector/ClipCollectorApplication.java
+++ src/main/java/de/tsearch/clipcollector/ClipCollectorApplication.java
@@ -1,6 +1,5 @@
 package de.tsearch.clipcollector;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableBatchProcessing
 @SpringBootApplication
 public class ClipCollectorApplication {
 
--- src/main/java/de/tsearch/clipcollector/task/CheckClipsTask.java
+++ src/main/java/de/tsearch/clipcollector/task/CheckClipsTask.java
@@ -1,38 +1,58 @@
 package de.tsearch.clipcollector.task;
 
+import de.tsearch.clipcollector.database.postgres.converter.TClipConverter;
+import de.tsearch.clipcollector.database.postgres.repository.ClipRepository;
+import de.tsearch.tclient.ClipClient;
+import de.tsearch.tclient.http.respone.Clip;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class CheckClipsTask {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private final JobLauncher jobLauncher;
+    private final ClipRepository clipRepository;
 
-    private final Job job;
+    private final ClipClient clipClient;
 
-    public CheckClipsTask(JobLauncher jobLauncher, @Qualifier("updateAllClipsJob") Job job) {
-        this.jobLauncher = jobLauncher;
-        this.job = job;
+    private final TClipConverter tClipConverter;
+
+    public CheckClipsTask(ClipRepository clipRepository, ClipClient clipClient, TClipConverter tClipConverter) {
+        this.clipRepository = clipRepository;
+        this.clipClient = clipClient;
+        this.tClipConverter = tClipConverter;
     }
 
     @Scheduled(fixedRate = 24 * 60 * 60 * 1000, initialDelay = 3 * 60 * 60 * 1000)
     protected void checkAllClips() {
         logger.info("Start check all existing clips job");
-        try {
-            jobLauncher.run(job, new JobParametersBuilder().toJobParameters());
-        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
-            e.printStackTrace();
+        List<String> ids = clipRepository.getAllIds();
+
+        final int batchSize = 1_000;
+        for (int round = 0; round < Math.ceil(((float) ids.size()) / batchSize); round++) {
+            logger.debug("Check clips starting index {} from {}", round * batchSize, ids.size());
+            List<String> list = ids.subList(round * batchSize, Math.min(ids.size(), (round + 1) * batchSize));
+            checkClipTask(list);
         }
+        logger.info("Finishing check all existing clips job");
+    }
+
+    private void checkClipTask(List<String> ids) {
+        List<Clip> activeClips = clipClient.getAllActiveClipsUncached(ids);
+        for (Clip activeClip : activeClips) {
+            Optional<de.tsearch.clipcollector.database.postgres.entity.Clip> clipOptional = clipRepository.findById(activeClip.getId());
+            if (clipOptional.isEmpty()) continue;
+            tClipConverter.updateDatabaseClipProperties(clipOptional.get(), activeClip);
+        }
+
+        //Delete old clips
+        List<String> toDeleteIds = ids.stream().filter(id -> activeClips.stream().noneMatch(clip -> id.equals(clip.getId()))).toList();
+        clipRepository.deleteAllById(toDeleteIds);
+        logger.debug("To delete clip ids: {}", toDeleteIds);
     }
 }
